a019c0960657f7176c821cc8a4cfa08111308c09
Dorothy Rifai <drifai28@gmail.com>
2020-07-28 18:27:22 -0400

    Added validation transitions.

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
@@ -26,4 +27,23 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
 		.end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
 	}
 	
-}
+
+	
+		@Override
+		public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+			transitions.withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW).event(BeerOrderEventEnum.VALIDATE_ORDER)
+			.event(BeerOrderEventEnum.VALIDATE_ORDER)
+			// todo add validation action here
+			.and().withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
+			.event(BeerOrderEventEnum.VALIDATION_PASSED)	
+			.and().withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+			.event(BeerOrderEventEnum.VALIDATION_FAILED);
+			//.and().withExternal().source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.ALLOCATED)
+			//.event(BeerOrderEventEnum.ALLOCATION_SUCCESS)
+			//.and().withExternal().source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.ALLOCATION_EXCEPTION)
+			//.event(BeerOrderEventEnum.ALLOCATION_FAILED)
+			//.and().withExternal().source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.DELIVERED)
+			//.event(BeerOrderEventEnum.)
+		}
+		
+}	
